0d4f85c0db1880a5dc91e0d09a32b56858ef9d68
monica445 <monicashalom86@gmail.com>
2026-03-12 15:17:11 +0700

    BLA-200 - Start Code Week 8

---
+++ lib/week8-before-practice/lib/services/location_service.dart
@@ -0,0 +1,11 @@
+import '../model/ride/locations.dart';
+import '../data/dummy_data.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+  static const List<Location> availableLocations =
+      fakeLocations; 
+}
